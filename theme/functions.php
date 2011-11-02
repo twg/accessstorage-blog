3d61f9ad0ecd281ae9449652cdffe8717a882ab7
Stephen McLeod <stephen.m.mcleod@gmail.com>
2011-11-02 14:34:31 -0400

    update search

--- theme/functions.php
+++ theme/functions.php
@@ -147,6 +147,20 @@ function twentyeleven_comment( $comment, $args, $depth ) {
 endif; // ends check for twentyeleven_comment()
 
 
+/**
+ * Display navigation to next/previous pages when applicable
+ */
+function twentyeleven_content_nav( $nav_id ) {
+	global $wp_query;
+
+	if ( $wp_query->max_num_pages > 1 ) : ?>
+		<nav id="<?php echo $nav_id; ?>">
+			<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyeleven' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></div>
+		</nav><!-- #nav-above -->
+	<?php endif;
+}
 
 
 ?>
